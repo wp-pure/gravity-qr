ff38f1de057301acaddedd22a77f76855b672995
James Pederson <james@jpederson.com>
2021-03-24 16:39:55 -0500

    Accept entry parameter as multipart string, split it, and use it appropriately.

--- generate.php
+++ generate.php
@@ -7,8 +7,12 @@ require_once("../../../wp-load.php");
 // include the library
 include 'lib/qrcode.php';
 
+// split the entry parameter into parts to use in the URL.
+$entry_part = explode( '::', $_REQUEST['entry'] );
+
+
 // spin up the generator and generate a qr code
-$generator = new QRCode( get_bloginfo( 'home' ) . '/wp-admin/admin.php?page=gf_entries&view=entry&id=' . $_REQUEST['id'] . '&lid=' . $_REQUEST['lid'] . '&order=ASC&filter&paged=1&pos=0&field_id&operator', $options );
+$generator = new QRCode( get_bloginfo( 'home' ) . '/wp-admin/admin.php?page=gf_entries&view=entry&id=' . $entry_part[0] . '&lid=' . $entry_part[1] . '&order=ASC&filter&paged=1&pos=0&field_id&operator', $options );
 
 
 /* Output directly to standard output. */
