667f03954ad221a76e3dc377672474c0cbcf1bec
James Pederson <james@jpederson.com>
2021-03-24 17:58:37 -0500

    Update to allow for 'entry' url parameter that overrides the 'data' one and makes it work in gravity forms.

--- generate.php
+++ generate.php
@@ -8,15 +8,25 @@ require_once("../../../wp-load.php");
 include 'lib/qrcode.php';
 
 
+// check to see if we're being passed gravity forms entry info
+if ( isset( $_REQUEST['entry'] ) ) {
+
+	// split the parameter
+	$entry_part = explode( '::', $_REQUEST['entry'] )
+
+	// build the data url.
+	$qr_data = get_bloginfo( 'home' ) . '/wp-admin/admin.php?page=gf_entries&view=entry&id=' . $entry_part[0] . '&lid=' . $entry_part[1] . '&order=ASC&filter&paged=1&pos=0&field_id&operator';
+
+} else {
+
+	// otherwise, directly put the data parameter into the qr data variable
+	$qr_data = $_REQUEST['data'];
+	
+}
+
+
 // spin up the generator and generate a qr code
-$generator = new QRCode( $_REQUEST['data'], array(
-	'w' => 200,
-	'h' => 200,
-	'pt' => 10,
-	'pr' => 10,
-	'pb' => 10,
-	'pl' => 10,
-	'sf' => 8,
+$generator = new QRCode( $qr_data, array(
 	'fc' => '#6A0A1B'
 ) );
 
--- readme.md
+++ readme.md
@@ -13,13 +13,17 @@ git clone https://github.com/wp-pure/qr
 To embed a QR code in a confirmation email from any Gravity Form, paste the following into the 'Text' editor for that confirmation:
 
 ```
-<img src="https://MYSITE.COM/wp-content/plugins/qr/generate.php?data={entry_url}" />
+<img src="https://MYSITE.COM/wp-content/plugins/qr/generate.php?entry={form_id}::{entry_id}" />
 ```
 
 Do remember to replace the `MYSITE.COM` part with your actual domain name, and everything will just work.
 
 ### General QR Code Generation
 You can also use this to generate ANY QR code you like - just feed the content of that QR code into the `data` parameter of the URL, and it'll generate the QR code for you.
+
+```
+<img src="https://MYSITE.COM/wp-content/plugins/qr/generate.php?data=DATA" />
+```
 
 *****
 Developed with love by [James Pederson](https://jpederson.com).
