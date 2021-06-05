3a447ce3c84e842e1149a0df23d5cf14ce94e120
James Pederson <james@jpederson.com>
2021-06-05 11:48:22 -0500

    Update plugin page link so it goes to the repo.

--- qr.php
+++ qr.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: QR Code Generator
-Plugin URI: https://github.com/jpederson/gravity-qr
+Plugin URI: https://github.com/wp-pure/qr
 description: A simple WordPress plugin that generates QR codes for entries in Gravity Forms - just feed an entry ID into the generate script and it'll generate a url QR code that will open the entry in your WP dashboard. Useful for if your gravity form entries are 'tickets' for events, and you want to quickly check a code for its corresponding entry.
 Version: 0.1.0
 Author: James Pederson
